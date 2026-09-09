be47e88189740958531381320250b7d3abc31786
Caleigh Charlebois <cwc@zoology.ubc.ca>
2026-09-09 13:11:07 -0700

    Add more comments

--- tools/fastize.sh
+++ tools/fastize.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Input: "ngs" format table from Tandem Repeat Finder
+# Output: Fasta with unique headers for each repeat, named with the read and
+# count on that read.
+
 awk '/^@/ {current_header=$1} !/^@/ {print ">" current_header "_" ++count[current_header] "\n" $14}' "$1"
 
--- tools/tandemize.sh
+++ tools/tandemize.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#Input: "ngs" style table output from Tandem Repeat Finder
+#Output: Fasta format file with headers being the read the monomer is from,
+#plus a number for uniqueness. Body for each read is the monomer itself, but
+#doubled in this file to tandemize so that blast can match other monomers to
+#parts of the sequence that might wrap around the edge of this monomer. This
+#is described in Melters et al. 2013.
+
 awk '/^@/ {current_header=$1} !/^@/ {print ">" current_header "_" ++count[current_header] "\n" $14 $14}' "$1"
 
