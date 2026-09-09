5b8da2047585c27756d8dcccfbdfc2a9baa711cc
Caleigh Charlebois <cwc@zoology.ubc.ca>
2026-09-09 13:22:39 -0700

    Add simple filtering command

---
+++ tools/simple_blast_filtering.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Input: Special BLAST+ result tsv where col 1 is query ID, col 2 is
+# database ID, and col 6 is percent of query covered by alignment.
+
+# Output: Same BLAST+ result table but without self-matches and 
+# matches covering 50% or less of the query. This is the filtering
+# used in Melters et al. (2013) "global" mode.
+
+awk '$1 != $2 && $6 > 50' "$1"
+
